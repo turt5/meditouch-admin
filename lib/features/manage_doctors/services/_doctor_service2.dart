a9e675ca22c58a4e732a6ea6a5e1ee5e90c85a92
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-12 02:55:01 +0600

    refactor DoctorService2 to use a Future instead of a Stream for fetching doctors from Firestore

--- lib/features/manage_doctors/services/_doctor_service2.dart
+++ lib/features/manage_doctors/services/_doctor_service2.dart
@@ -5,10 +5,11 @@ import '../models/_doctor_model.dart';
 class DoctorService2 {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Stream<List<DoctorModel>> getDoctors() {
-    return _firestore.collection('doctors').snapshots().map((snapshot) =>
-        snapshot.docs
-            .map((doc) => DoctorModel.fromMap(doc.data(), doc.id))
-            .toList());
+  Future<List<DoctorModel>> getDoctors() async {
+    final snapshot =
+        await _firestore.collection('db_client_doctor_accountinfo').get();
+    return snapshot.docs
+        .map((doc) => DoctorModel.fromMap(doc.data(), doc.id))
+        .toList();
   }
 }
