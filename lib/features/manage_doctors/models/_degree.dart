b0e3d5cae2750953cb3e7b97f2b00c8e71230135
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-12 02:54:53 +0600

    refactor Degree model to use 'passedYear' for year field and improve formatting

--- lib/features/manage_doctors/models/_degree.dart
+++ lib/features/manage_doctors/models/_degree.dart
@@ -1,40 +1,35 @@
-class Degree{
+class Degree {
   final String year;
   final String degree;
   final String institution;
 
   Degree({required this.year, required this.degree, required this.institution});
 
-  factory Degree.fromMap(Map<String, dynamic> data){
-    final year = data['year'];
+  factory Degree.fromMap(Map<String, dynamic> data) {
+    final year = data['passedYear'];
     final degree = data['degree'];
     final institution = data['institution'];
     return Degree(year: year, degree: degree, institution: institution);
   }
 
-  Map<String, dynamic> toMap(){
-    return {
-      'year': year,
-      'degree': degree,
-      'institution': institution
-    };
+  Map<String, dynamic> toMap() {
+    return {'passedYear': year, 'degree': degree, 'institution': institution};
   }
 
-
-
+  @override
+  String toString() =>
+      'Degree(year: $year, degree: $degree, institution: $institution)';
 
   @override
-  String toString() => 'Degree(year: $year, degree: $degree, institution: $institution)';
-
-  @override
-  bool operator ==(Object other){
-    if(identical(this, other)) return true;
-    if(other.runtimeType != runtimeType) return false;
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    if (other.runtimeType != runtimeType) return false;
     final Degree otherDegree = other as Degree;
-    return otherDegree.year == year && otherDegree.degree == degree && otherDegree.institution == institution;
+    return otherDegree.year == year &&
+        otherDegree.degree == degree &&
+        otherDegree.institution == institution;
   }
 
   @override
   int get hashCode => year.hashCode ^ degree.hashCode ^ institution.hashCode;
-
 }
