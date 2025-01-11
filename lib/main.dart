8a09014407f75d2bf80185dbd6c9f1ff148f3d27
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-12 02:27:02 +0600

    refactor main.dart to integrate GetX controllers for improved state management

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,20 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get/get.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:meditouch_admin/core/theming/_theme.dart';
 import 'package:meditouch_admin/features/dashboard_navigation/views/_admin_dashboard.dart';
+import 'package:meditouch_admin/features/login/controller/login_controller.dart';
 import 'package:meditouch_admin/features/login/views/_loginpage2.dart';
 import 'package:meditouch_admin/firebase_options.dart';
 import 'package:meditouch_admin/shared/local_db/_db_helper.dart';
 import 'package:meditouch_admin/shared/local_db/person.dart';
 
-import 'features/login/views/_loginpage.dart';
+import 'app/meditouch_admin.dart';
+import 'features/add_doctor/controller/add_doctor_controller.dart';
+import 'features/dashboard_navigation/controller/navigation_controller.dart';
 
 void main() async {
   // Ensure that the Flutter binding is initialized before calling the runApp() function.
@@ -29,30 +33,17 @@ void main() async {
 
   final Person? person = dbHelper.getUser();
 
+  // inject login controller
+  Get.put(LoginController());
+
+  // inject navigation controller
+  Get.put(NavigationController());
+
+  // inject add doctor controller
+  Get.put(AddDoctorController());
+
   // Run the app
-  runApp(ProviderScope(
-      child: MyApp(
+  runApp(MeditouchAdmin(
     person: person,
-  )));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key, required this.person});
-
-  final Person? person;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: AppTheme().getTheme(),
-      home: person != null &&
-              person!.id.isNotEmpty &&
-              person!.role.isNotEmpty &&
-              person!.role == 'a'
-          ? AdminDashboard()
-          : LoginPage2(),
-    );
-  }
+  ));
 }
